f4eb365e3347c0dd86cd48df67486ba3f9d78b94
Zoro4rk <thuyetngocluong@gmail.com>
2024-04-18 01:52:30 +0700

    Initial Commit

---
+++ ios_translate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ios_translate
+//
+//  Created by ThuyetLN on 18/4/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
